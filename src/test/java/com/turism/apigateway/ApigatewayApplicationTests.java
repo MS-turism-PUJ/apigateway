ce39dd1fb5025a4d0bc6a4eebc6fc306413f7d6d
Santicm23 <santicastrom@outlook.com>
2024-08-25 13:36:26 -0500

    starting project

---
+++ src/test/java/com/turism/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turism.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
